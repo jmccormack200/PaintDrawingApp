8a97b57b0db4fffb5e0d7181a26f95b6cecb4b17
John McCormack <mccormack.wgsi@gmail.com>
2016-06-01 13:43:44 -0400

    Working towards animation on the FAB Speedial

--- app/src/main/java/com/example/jmack/paint/MainActivity.java
+++ app/src/main/java/com/example/jmack/paint/MainActivity.java
@@ -4,7 +4,12 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.Window;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
 
     private TypedArray styledAttributes;
     private int mActionBarSize;
+
+    //TODO: Convert this to a list
+
+    private ArrayList<View> paletteArrayList;
 
 
     @Override
@@ -26,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        final ViewGroup fabContainer = (ViewGroup) findViewById(R.id.sdpallete);
+        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.palette_parent);
+
+        collapseFAB(fabContainer, paletteArrayList, rl);
 
         mDrawView = (DrawView) findViewById(R.id.drawview);
     }
@@ -44,4 +58,26 @@ public class MainActivity extends AppCompatActivity {
         mDrawView.clear();
     }
 
+    public void collapseFAB(final ViewGroup fabContainer,
+                            final ArrayList<View> fabArrayList,
+                            RelativeLayout rl){
+
+        //TODO I think we can remove this part
+        for (int i=0; i < rl.getChildCount(); i++) {
+            fabArrayList.add(rl.getChildAt(i));
+        }
+
+        fabContainer.getViewTreeObserver()
+                .addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener(){
+            @Override
+            public boolean onPreDraw(){
+                fabContainer.getViewTreeObserver().removeOnPreDrawListener(this);
+                for(int i = 0; i < fabArrayList.size() - 1; i++){
+                    float offset = fabArrayList.get(i).getY() - fabArrayList.get(i + 1).getY();
+                    fabArrayList.get(i + 1).setTranslationY(offset);
+                }
+                return true;
+            }
+        });
+    }
 }
