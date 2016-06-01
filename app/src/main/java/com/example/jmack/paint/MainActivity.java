541fd9530fbed076f3a2fb260976ce31a2a66977
John McCormack <mccormack.wgsi@gmail.com>
2016-06-01 15:22:10 -0400

    Planning to refactor in order to link the offsets to the view in the array

--- app/src/main/java/com/example/jmack/paint/MainActivity.java
+++ app/src/main/java/com/example/jmack/paint/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.jmack.paint;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.Window;
-import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -18,9 +21,13 @@ public class MainActivity extends AppCompatActivity {
     private TypedArray styledAttributes;
     private int mActionBarSize;
 
+    private boolean collapsePal = true;
+    private ViewGroup paletteContainer;
+    private View rootPalette;
+
     //TODO: Convert this to a list
 
-    private ArrayList<View> paletteArrayList;
+    private ArrayList<View> paletteArrayList = new ArrayList<>();
 
 
     @Override
@@ -36,13 +43,14 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        final ViewGroup fabContainer = (ViewGroup) findViewById(R.id.sdpallete);
-        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.palette_parent);
+        paletteContainer = (ViewGroup) findViewById(R.id.sdpallete);
+        rootPalette = paletteContainer.getChildAt(paletteContainer.getChildCount() -1);
+        initCollapseFAB(paletteContainer, paletteArrayList);
 
-        collapseFAB(fabContainer, paletteArrayList, rl);
 
         mDrawView = (DrawView) findViewById(R.id.drawview);
     }
+
 
     @Override
     public void onResume(){
@@ -58,13 +66,12 @@ public class MainActivity extends AppCompatActivity {
         mDrawView.clear();
     }
 
-    public void collapseFAB(final ViewGroup fabContainer,
-                            final ArrayList<View> fabArrayList,
-                            RelativeLayout rl){
+    public void initCollapseFAB(final ViewGroup fabContainer,
+                                final ArrayList<View> fabArrayList){
 
         //TODO I think we can remove this part
-        for (int i=0; i < rl.getChildCount(); i++) {
-            fabArrayList.add(rl.getChildAt(i));
+        for (int i=0; i < fabContainer.getChildCount(); i++) {
+            fabArrayList.add(fabContainer.getChildAt(i));
         }
 
         fabContainer.getViewTreeObserver()
@@ -72,12 +79,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onPreDraw(){
                 fabContainer.getViewTreeObserver().removeOnPreDrawListener(this);
-                for(int i = 0; i < fabArrayList.size() - 1; i++){
-                    float offset = fabArrayList.get(i).getY() - fabArrayList.get(i + 1).getY();
-                    fabArrayList.get(i + 1).setTranslationY(offset);
+                for(int i = (fabArrayList.size() - 1); i > 0; i--){
+                    Log.v("HELP", String.valueOf(i));
+                    float offset = fabArrayList.get(i).getY() - fabArrayList.get(i - 1).getY();
+                    fabArrayList.get(i - 1).setTranslationY(offset);
                 }
                 return true;
             }
         });
     }
+
+    public void onClickExpand(View view){
+        collapsePal = !collapsePal;
+        if (collapsePal) {
+            expandFAB();
+        } else {
+            //collapseFAB();
+        }
+    }
+
+    private static final String TRANSLATION_Y = "translationY";
+
+    private Animator createCollapseAnimator(View view, float offset){
+        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, 0, offset)
+                .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+    }
+
+    private Animator createExpandAnimator(View view, float offset){
+        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, offset, 1)
+                .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+    }
+
+    private void expandFAB(){
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ArrayList<Animator> animatorArrayList = new ArrayList<>();
+        animatorSet.playTogether();
+    }
 }
+
+
