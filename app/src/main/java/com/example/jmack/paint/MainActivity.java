f64bd1d24d661eb0397d99b561f0e7c9aa0cd8a0
John McCormack <mccormack.wgsi@gmail.com>
2016-06-03 13:50:12 -0400

    have the animation nearly working, there is a slight overshoot when everything collapses

--- app/src/main/java/com/example/jmack/paint/MainActivity.java
+++ app/src/main/java/com/example/jmack/paint/MainActivity.java
@@ -4,9 +4,11 @@ import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.res.ColorStateList;
+import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -31,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final float strokeWidth = 8.0f;
     private static final String TRANSLATION_Y = "translationY";
+    private static final String TRANSLATION_X = "translationX";
+
+    private String mTranslation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            mTranslation = TRANSLATION_Y;
+        } else {
+            mTranslation = TRANSLATION_X;
+        }
+
         paletteContainer = (ViewGroup) findViewById(R.id.sdpallete);
         initCollapseFAB(paletteContainer, paletteArrayList);
 
@@ -54,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
 
         mDrawView = (DrawView) findViewById(R.id.drawview);
     }
-
 
     @Override
     public void onResume(){
@@ -78,12 +88,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onPreDraw(){
                 fabContainer.getViewTreeObserver().removeOnPreDrawListener(this);
+
                 for(int i = (fabContainer.getChildCount() - 1); i > 0; i--){
-                    float offset = fabContainer.getChildAt(i).getY()
-                            - fabContainer.getChildAt(i - 1).getY();
-                    fabContainer.getChildAt(i - 1).setTranslationY(offset);
+                    View button = fabContainer.getChildAt(i);
+                    View prevButton = fabContainer.getChildAt(i - 1);
+                    float offset;
+                    if (mTranslation == TRANSLATION_Y) {
+                        offset = button.getY() - prevButton.getY();
+                        prevButton.setTranslationY(offset);
+                    } else {
+                        offset = button.getX() - prevButton.getX();
+                        prevButton.setTranslationX(offset);
+                    }
                     ViewOffsetHolder viewOffsetHolder =
-                            new ViewOffsetHolder(fabContainer.getChildAt(i-1), offset);
+                            new ViewOffsetHolder(fabContainer.getChildAt(i - 1), offset);
                     fabArrayList.add(viewOffsetHolder);
                 }
                 return true;
@@ -126,19 +144,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Animator createCollapseAnimator(View view, float offset){
-        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, 0, offset)
+        Log.v("Test", mTranslation);
+        return ObjectAnimator.ofFloat(view, mTranslation, 0, offset)
                 .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
     }
 
     private Animator createExpandAnimator(View view, float offset){
-        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, offset, 1)
+        return ObjectAnimator.ofFloat(view, mTranslation, offset, 1)
                 .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
     }
 
     //TODO It should be possible to collapse these two into one function
     private void expandFAB(ArrayList<ViewOffsetHolder> viewOffsetHolderArrayList){
         AnimatorSet animatorSet = new AnimatorSet();
-
 
         ArrayList<Animator> animatorArrayList = new ArrayList<>();
         for (int i = 0; i < viewOffsetHolderArrayList.size(); i++){
